f4c37efd8b6ef21fd3270b64e5ba66394d73b8c2
Clover <Clover@DEVREL-CLOVER>
2017-03-09 13:54:27 -0800

    Add project files.

---
+++ remote-pay-windows-hello-world/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace remote_pay_windows_hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
