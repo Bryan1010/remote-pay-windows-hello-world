d261d1bcd9554e5efcc8612ddf6d2e44e3d4f5a0
Bryan Cruz <bryanc@fromuthtennis.com>
2017-03-13 10:13:51 -0400

    bryan initial commit

--- remote-pay-windows-hello-world/Program.cs
+++ remote-pay-windows-hello-world/Program.cs
@@ -1,80 +1,77 @@
-﻿using System;
-using com.clover.remotepay.sdk;
-using com.clover.remotepay.transport;
-using System.Threading;
+﻿//using System;
+//using com.clover.remotepay.sdk;
+//using com.clover.remotepay.transport;
+//using System.Threading;
 
-namespace remote_pay_windows_hello_world
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            ICloverConnector cloverConnector;
-            Console.Write("initializing");
-            CloverDeviceConfiguration USBConfig = new USBCloverDeviceConfiguration("__deviceID__", "thebrewery.tech.test", false, 1);
+//namespace remote_pay_windows_hello_world
+//{
+//    class Program
+//    {
+//        static void Main1(string[] args)
+//        {
+//            ICloverConnector cloverConnector;
+//            Console.Write("initializing");
+//            CloverDeviceConfiguration USBConfig = new USBCloverDeviceConfiguration("__deviceID__", "thebrewery.tech.test", false, 1);
 
-            cloverConnector = new CloverConnector(USBConfig);
-            cloverConnector.AddCloverConnectorListener(new YourListener(cloverConnector));
-            cloverConnector.InitializeConnection();
+//            cloverConnector = new CloverConnector(USBConfig);
+//            cloverConnector.AddCloverConnectorListener(new YourListener(cloverConnector));
+//            cloverConnector.InitializeConnection();
 
-            Thread.Sleep(7000);
+//            Thread.Sleep(7000);
 
-            cloverConnector.ShowMessage("hello world");
+//            cloverConnector.ShowMessage("hello world");
 
-            Thread.Sleep(7000);
+//            Thread.Sleep(7000);
 
-            Console.Write("finishing");
-        }
+//            Console.Write("finishing");
+//        }
 
-        class YourListener : DefaultCloverConnectorListener
-        {
-            ICloverConnector cloverConnector;
-            public YourListener(ICloverConnector cc) : base(cc) 
-            {
-                cloverConnector = cc;
-            }
+//        class YourListener : DefaultCloverConnectorListener
+//        {
+//            ICloverConnector cloverConnector;
+//            public YourListener(ICloverConnector cc) : base(cc)
+//            {
+//                cloverConnector = cc;
+//            }
 
-            public override void OnConfirmPaymentRequest(ConfirmPaymentRequest request)
-            {
-                // there are only two possible challenges - DUPLICATE_CHALLENGE and OFFLINE_CHALLENGE 
-                for (int i = 0; i < request.Challenges.Count; i++)
-                {
-                    if (request.Challenges[i].type == ChallengeType.DUPLICATE_CHALLENGE)
-                    {
-                        // handle the challenge, "This might be a duplicate payment, continue?"
-                    }
-                    
-                    if (request.Challenges[i].type == ChallengeType.OFFLINE_CHALLENGE)
-                    {
-                        // handle an offline payment request challenge
-                    }
-                }
+//            public override void OnConfirmPaymentRequest(ConfirmPaymentRequest request)
+//            {
+//                for (int i = 0; i < request.Challenges.Count; i++)
+//                {
+//                    if (request.Challenges[i].type == ChallengeType.DUPLICATE_CHALLENGE)
+//                    {
+//                        // handle the challenge, "This might be a duplicate payment, continue?"
+//                    }
 
-           
-                // for the sake of a hello world demo, just accept all payments
-                this.cloverConnector.AcceptPayment(request.Payment);
-            }
-
-            public override void OnSaleResponse(SaleResponse response)
-            {
-                if (response.Success)
-                {
-                    // payment was successful
-                    // do something with response.Payment
-                }
-                else
-                {
-                    // payment didn't complete, can look at response.Result, response.Reason for additional info
-                }
-            }
-
-            // wait until this gets called to indicate the device
-            // is ready to communicate before calling other methods
-            public override void OnDeviceReady(MerchantInfo merchantInfo)
-            {
-            }
-        }
-    }
-}
+//                    if (request.Challenges[i].type == ChallengeType.OFFLINE_CHALLENGE)
+//                    {
+//                        // handle an offline payment request challenge
+//                    }
+//                }
 
 
+//                // for the sake of a hello world demo, just accept all payments
+//                this.cloverConnector.AcceptPayment(request.Payment);
+//            }
+
+//            public override void OnSaleResponse(SaleResponse response)
+//            {
+//                if (response.Success)
+//                {
+//                    // payment was successful
+//                    // do something with response.Payment
+//                }
+//                else
+//                {
+//                    // payment didn't complete, can look at response.Result, response.Reason for additional info
+//                }
+//            }
+
+//            // wait until this gets called to indicate the device
+//            // is ready to communicate before calling other methods
+//            public override void OnDeviceReady(MerchantInfo merchantInfo)
+//            {
+//            }
+//        }
+//    }
+//}
---
+++ remote-pay-windows-hello-world/programCopy.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using com.clover.remotepay.sdk;
+using com.clover.remotepay.transport;
+using System.Threading;
+
+namespace remote_pay_windows_hello_world
+{
+    class ProgramCopy
+    {
+        static void Main(string[] args)
+        {
+            int amountCharged = 0103;
+            string invoiceNumber = "aa123f";
+            ICloverConnector cloverConnector;
+            Console.Write("initializing");
+            CloverDeviceConfiguration USBConfig = new USBCloverDeviceConfiguration(null, "com.fromuthtennis.ustaproshop", false, 0000);
+
+            cloverConnector = new CloverConnector(USBConfig);
+            cloverConnector.AddCloverConnectorListener(new YourListener(cloverConnector));
+            cloverConnector.InitializeConnection();
+
+
+            Thread.Sleep(3000);
+            if (cloverConnector.IsReady)
+            {
+
+
+            }
+
+            else Console.Write("not connected");
+            SaleResponse sresponse = new SaleResponse();
+
+            //sresponse.Signature();
+            VerifySignatureRequest t = new VerifySignatureRequest();
+
+
+
+            AuthRequest arequest = new AuthRequest();
+
+
+
+
+
+            Thread.Sleep(999999900);
+
+            //Console.Write("finishing");
+            cloverConnector.RemoveCloverConnectorListener(new YourListener(cloverConnector));
+            //Environment.Exit(0);
+        }
+
+        class YourListener : DefaultCloverConnectorListener
+        {
+            ICloverConnector localcc;
+            public YourListener(ICloverConnector cc) : base(cc)
+            {
+                localcc = cc;
+            }
+
+            public override void OnConfirmPaymentRequest(ConfirmPaymentRequest request)
+            {
+                for (int i = 0; i < request.Challenges.Count; i++)
+                {
+                    if (request.Challenges[i].type == ChallengeType.DUPLICATE_CHALLENGE)
+                    {
+                        // handle the challenge, "This might be a duplicate payment, continue?"
+                    }
+
+                    if (request.Challenges[i].type == ChallengeType.OFFLINE_CHALLENGE)
+                    {
+                        // handle an offline payment request challenge
+                    }
+                }
+
+
+                // for the sake of a hello world demo, just accept all payments
+                this.localcc.AcceptPayment(request.Payment);
+            }
+
+            public override void OnVerifySignatureRequest(VerifySignatureRequest request)
+            {
+                request.Accept();
+            }
+
+
+
+            public override void OnSaleResponse(SaleResponse response)
+            {
+
+                if (response.Success)
+                {
+                    localcc.ResetDevice();
+                    Console.Write("fasndf\nadjfh\nasdfjkha\nasjldf\r\naskjdfh");
+                }
+                else
+                {
+                    localcc.ShowMessage("Order Canceled");
+                    Thread.Sleep(3000);
+                    localcc.ShowWelcomeScreen();
+
+                }
+            }
+
+            // wait until this gets called to indicate the device
+            // is ready to communicate before calling other methods
+            public override void OnDeviceReady(MerchantInfo merchantInfo)
+            {
+                localcc.ResetDevice();
+                int amountCharged = 503;
+                string invoiceNumber = "aa1231";
+                SaleRequest sarequest = new SaleRequest();
+                sarequest.Amount = amountCharged;
+                sarequest.ExternalId = invoiceNumber; //KAD46SD3SFR7P
+
+                sarequest.CardEntryMethods = CloverConnector.CARD_ENTRY_METHOD_MANUAL;
+                sarequest.CardEntryMethods |= CloverConnector.CARD_ENTRY_METHOD_MAG_STRIPE;
+                sarequest.CardEntryMethods |= CloverConnector.CARD_ENTRY_METHOD_ICC_CONTACT;
+                sarequest.CardEntryMethods |= CloverConnector.CARD_ENTRY_METHOD_NFC_CONTACTLESS;
+
+                sarequest.DisablePrinting = true;
+                sarequest.ApproveOfflinePaymentWithoutPrompt = true;
+                sarequest.AllowOfflinePayment = true;
+
+
+                localcc.Sale(sarequest);
+                VerifySignatureMessage a = new VerifySignatureMessage();
+                VerifySignatureRequest vsrequest = new VerifySignatureRequest();
+                localcc.AcceptSignature(vsrequest);
+            }
+        }
+    }
+}
+
+
